aa7face7a2d4f3f31334b348f365dd8534b4b79c
Santirodriguezb <santiagorodriguezb@ieslaxeiro.com>
2022-01-14 09:28:48 +0100

    Operadores aritméticos

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contrución de expresións en PHP</title>
+</head>
+<body>
+    <h3>Construción de expresións en PHP</h3>
+
+    <?php
+    //Operadores aritméticos
+    $x=10;
+    $y=3;
+    $z=$x * $y;
+    echo $x." x ".$y." = ".$z."<br/>";
+    $z=$x % $y;
+    echo $x." módulo ".$y." = ".$z."<br/>";
+    ?>
+</body>
+</html>
